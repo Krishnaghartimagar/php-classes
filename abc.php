e2b8d7e8f5770b728a7bb76f334259802ae595de
Krishnaghartimagar <unknown420magar@gmail.com>
2025-12-07 13:04:05 +0545

    php classes practices

---
+++ abc.php
@@ -0,0 +1,11 @@
+<?php
+// $data=$_GET;
+// echo "data";
+// echo "GET";
+// var_dump(value:$_GET)
+// echo "REQUEST"
+// var_dump(VALUE:$_REQUEST);
+// echo "post";
+// var_dump($_POST);
+echo $_GET['name'];
+?>
---
+++ form2.php
@@ -0,0 +1,85 @@
+
+ <?php
+
+
+echo '
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Simple Form</title>
+    <style>
+    .myclass{
+    width:400px;
+    height:25px;
+    background-color:rgba(219, 223, 223, 1);
+    border-radius:10px
+}
+    .mydiv{
+    width:800px;
+    margin:auto;
+
+    
+    padding:50px 50px 50px 200px;
+}
+    input{
+    
+    color:blue;
+    margin:10px;
+}
+    .mybtn{
+    height:40px;
+    width:150px;
+    border-radius:10px;
+    margin:10px;
+    </style>
+</head>
+<body>
+
+
+<div class="mydiv">
+<h2>Simple Form</h2>
+
+<form action="abc.php" method="post">
+
+    <label>Name:</label><br>
+    <input class="myclass" type="text" name="name" required><br><br>
+
+    <label>Phone:</label><br>
+    <input class="myclass" type="text" name="phone" required><br><br>
+
+    <label>Email:</label><br>
+    <input class="myclass" type="email" name="email" required><br><br>
+
+    <label>Password:</label><br>
+    <input class="myclass" type="password" name="password" required><br><br>
+
+    <label>Gender:</label><br>
+    <input type="radio" name="gender" value="male"> Male
+    <input type="radio" name="gender" value="female"> Female
+    <input type="radio" name="gender" value="other"> Other<br><br>
+
+    <label>Hobbies:</label><br>
+    <input type="checkbox" name="hobby" value="reading"> Reading
+    <input type="checkbox" name="hobby" value="sports"> Sports
+    <input type="checkbox" name="hobby" value="music"> Music
+    <br><br>
+
+    <label>Subject:</label><br>
+    <select name="Subject">
+        <option value="">--Select--</option>
+        <option value="POM">POM</option>
+        <option value="PHP">PHP</option>
+        <option value="DBMS">DBMS</option>
+        <option value="Linear">Linear alzebra</option>
+        <option value="OOA">OOA</option>
+    </select><br><br>
+
+    <button class="mybtn" type="submit">Submit</button>
+
+</form>
+</div>
+
+</body>
+</html>
+';
+?>
